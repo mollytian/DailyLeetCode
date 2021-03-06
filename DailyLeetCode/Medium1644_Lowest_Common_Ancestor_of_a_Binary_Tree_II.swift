abaae819515cf2f858346d018516d3cf574115b9
Mengyuan Tian <mollytian.mt@gmail.com>
2021-03-06 16:35:34 -0600

    Update challenge 1644

--- DailyLeetCode/Medium1644_Lowest_Common_Ancestor_of_a_Binary_Tree_II.swift
+++ DailyLeetCode/Medium1644_Lowest_Common_Ancestor_of_a_Binary_Tree_II.swift
@@ -8,31 +8,33 @@
 import Foundation
 
 class Medium1644_Lowest_Common_Ancestor_of_a_Binary_Tree_II {
-    public func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
-        //3. Exit: leaf
-        guard root != nil else {
-            return root
-        }
-        
-        // Exit: already found
-        if (root!.val == p!.val || root!.val == q!.val) {
-            return root
-        }
-        
-        //2. Divide: root左右找，满足两个都已找到的root则返回这个root
-        let left = lowestCommonAncestor(root!.left, p, q)
-        let right = lowestCommonAncestor(root!.right, p, q)
-        
-        //2.2 Conquer: 左右都找到 or 只找到一个 or 都没找到
-        if (left != nil && right != nil) {
-            return root
-        }
-        if (left != nil) {
-            return left
-        }
-        if (right != nil) {
-            return right
-        }
-        return nil
+    /*  Java Solution
+    int count = 0;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode res = helper(root, p, q);
+        return count == 2 ? res : null;
     }
+    
+    public TreeNode helper(TreeNode root, TreeNode p, TreeNode q) {
+        //3. Exit: leaf
+        if (root == null) {
+            return root;
+        }
+
+        //2. Divide: root左右找，满足两个都已找到的root则返回这个root
+        var left = helper(root.left, p, q);
+        var right = helper(root.right, p, q);
+        
+        if (root.val == p.val || root.val == q.val) {
+            count ++;
+            return root;
+        }
+        
+        //2.2 Conquer: 左右都找到 or 只找到一个
+        if (left != null && right != null) {
+            return root;
+        }
+        return (left != null) ? left : right;
+    }
+ */
 }
