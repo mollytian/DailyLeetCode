cf42fc44c4ddb5805b0ae63033860bae3ffc53e6
Mengyuan Tian <mollytian.mt@gmail.com>
2021-03-09 15:05:14 -0600

    Add Challenge253\252

---
+++ DailyLeetCode/DailyLeetCode/Easy252_Meeting_Rooms.swift
@@ -0,0 +1,32 @@
+//
+//  Easy252_Meeting_Rooms.swift
+//  DailyLeetCode
+//
+//  Created by Mengyuan Tian on 2021/3/9.
+//
+
+import Foundation
+
+class Easy252_Meeting_Rooms {
+    func canAttendMeetings(_ intervals: [[Int]]) -> Bool {
+        
+    }
+    
+    
+    /*Java
+     public boolean canAttendMeetings(Interval[] intervals) {
+         Arrays.sort(intervals, new Comparator<Interval>() {
+             @Override
+             public int compare(Interval o1, Interval o2) {
+                 return o1.start - o2.start;
+             }
+         });
+         for (int i = 1; i < intervals.length; i++) {
+             if (intervals[i].start < intervals[i - 1]. end) {
+                 return false;
+             }
+         }
+         return true;
+     }
+     */
+}
---
+++ DailyLeetCode/DailyLeetCode/Medium253_Meeting_Rooms_II.swift
@@ -0,0 +1,40 @@
+//
+//  Medium253_Meeting_Rooms_II.swift
+//  DailyLeetCode
+//
+//  Created by Mengyuan Tian on 2021/3/9.
+//
+
+import Foundation
+
+class Medium253_Meeting_Rooms_II {
+    func minMeetingRooms(_ intervals: [[Int]]) -> Int {
+        guard var intervals = intervals, intervals != 0 else {
+            return 0
+        }
+        
+    }
+    
+    /* Java
+    public int minMeetingRooms(Interval[] intervals) {
+        if(intervals == null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, (a, b) -> (a.start - b.start));
+        int max = 0;
+        PriorityQueue<Interval> queue = new PriorityQueue<>(intervals.length, (a, b) -> (a.end - b.end));
+        for(int i = 0; i < intervals.length; i++){
+            while(!queue.isEmpty() && intervals[i].start >= queue.peek().end)
+                queue.poll();
+            queue.offer(intervals[i]);
+            max = Math.max(max, queue.size());
+        }
+        return max;
+    }
+    */
+    
+    
+    
+    
+    
+    
+    
+}
